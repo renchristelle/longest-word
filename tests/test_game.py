b6881a28e1d2fab74603b7959fb363c6759bb697
Christelle Ren <christelle.ren@gmail.com>
2024-10-01 11:50:21 +0000

    Configure Github Actions CI to run pytest

--- tests/test_game.py
+++ tests/test_game.py
@@ -15,3 +15,35 @@ class TestGame:
             assert len(grid) == 9
             for letter in grid:
                 assert letter in string.ascii_uppercase
+
+
+    def test_empty_word_is_invalid(self):
+        # setup
+        new_game = Game()
+        # verify
+        assert new_game.is_valid('') is False
+
+
+    def test_is_valid(self):
+        # setup
+        new_game = Game()
+        test_grid = 'KWEUEAKRZ'
+        test_word = 'EUREKA'
+        # exercice
+        new_game.grid = list(test_grid) # Force the grid to a test case
+        # verify
+        assert new_game.is_valid(test_word) is True
+        # teardown
+        assert new_game.grid == list(test_grid) # Make sure the grid remained untouched
+
+    def test_is_invalid(self):
+        # setup
+        new_game = Game()
+        test_grid = 'KWEUEAKRZ'
+        test_word = 'SANDWICH'
+        # exerice
+        new_game.grid = list(test_grid) # Force the grid to a test case
+        # verify
+        assert new_game.is_valid(test_word) is False
+        # teardown
+        assert new_game.grid == list(test_grid) # Make sure the grid remained untouched
